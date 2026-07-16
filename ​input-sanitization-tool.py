1d5abc216f4bf90cbd03d1dad6a4eaccdc29041a
Umaimah <umaimahismail90@gmail.com>
2026-07-16 19:42:19 +0500

    Enhance data cleaning and input process
    Refactor data input handling and improve loop structure.

--- ​input-sanitization-tool.py
+++ ​input-sanitization-tool.py
@@ -1,14 +1,19 @@
 def data_cleaner(data):
     clean=[]
     for item in data:
-        try:
-            item=int(item)
-            if item >0:
-                clean.append(item)
-        except:
-            continue
+            try:
+                item=int(item)
+                if item >0:
+                    clean.append(item)
+            except:
+                 continue
     print(clean)
-
-Data=input("input the items in your list separated by spaces: ")
-raw=Data.split()
-data_cleaner(raw)
+    print("type done to exit")
+data=""
+while data!="done":
+     data=input("input:" )
+     data=data.lower()
+     if data=="done":
+         break
+     raw=data.split()
+     data_cleaner(raw)                    
